8590d0c315d396ff3de19c2bebff1e63b873c96a
MulanSales <luanmsacc@gmail.com>
2019-11-09 17:35:14 -0200

    added swagger documentation to project

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
         <h1>Desafios Extratos</h1>
         <h2>Badges</h2>
         <div>
-            <img hspace=5px src="https://travis-ci.org/MulanSales/DesafioExtratos.svg?branch=master"/>
+            <a href="https://travis-ci.org/MulanSales/DesafioExtratos"><img hspace=5px src="https://travis-ci.org/MulanSales/DesafioExtratos.svg?branch=master"/></a>
             <a  href="https://codeclimate.com/github/MulanSales/DesafioExtratos/maintainability"><img hspace=5px src="https://api.codeclimate.com/v1/badges/731b5d8ba6191fd92be6/maintainability" /></a>
             <a href='https://coveralls.io/github/MulanSales/DesafioExtratos?branch=master'><img hspace=5px src='https://coveralls.io/repos/github/MulanSales/DesafioExtratos/badge.svg?branch=master' alt='Coverage Status' /></a>
         </div>
--- src/ExtratosApi/Startup.cs
+++ src/ExtratosApi/Startup.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 namespace ExtratosApi
 {
@@ -26,11 +22,41 @@ namespace ExtratosApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Adding Swagger Generator Service
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "Extratos-Api", 
+                    Version = "v1",
+                    Description = "API para extratos pessoais",
+                    Contact = new OpenApiContact {
+                        Name = "Luan Sales",
+                        Email = "luanmsacc@gmail.com",
+                        Url = new Uri("https://github.com/MulanSales")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            // Add swagger documentation to pipeline
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Extrato-Api v1");
+                SubmitMethod[] methods = {
+                    SubmitMethod.Get, 
+                    SubmitMethod.Post, 
+                    SubmitMethod.Options, 
+                    SubmitMethod.Delete, 
+                    SubmitMethod.Delete,
+                    SubmitMethod.Put
+                };
+                c.SupportedSubmitMethods(methods);
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
